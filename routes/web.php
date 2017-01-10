61adfa536cbdc5f7e2e222b6f8920f8569d3a57f
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-10 16:48:06 +0100

    first 2017 commit

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('box', 'BoxController@index')->name('box');
+
     Route::get('tasks', 'TasksController@index')->name('tasks');
 
 });
