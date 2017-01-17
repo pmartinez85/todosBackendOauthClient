149be7d9792272bcee1e8cf96dcd89145903b3a4
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-17 17:03:04 +0100

    flex bg

--- resources/views/flexboxlayout.blade.php
+++ resources/views/flexboxlayout.blade.php
@@ -1,21 +1,29 @@
-@extends('adminlte::page')
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
 
-@section('htmlheader_title')
-	Change Title here!
-@endsection
+<style>
+
+	.article {
+		background-color: #636b6f;
+	}
+
+	
+</style>
+
+<body>
 
 
-@section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Change Header</div>
-					<div class="panel-body">
-						Change content here!
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-@endsection
+<div class="header">Flebox Layout Example</div>
+<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolor ex saepe sequi! Cupiditate ea inventore labore obcaecati quae quia?</div>
+<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolor ex saepe sequi! Cupiditate ea inventore labore obcaecati quae quia?</div>
+<div class="article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolor ex saepe sequi! Cupiditate ea inventore labore obcaecati quae quia?</div>
+
+</body>
+</html>
