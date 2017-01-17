a19f85db2b3904dc11d30e44634c27d8a7545271
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-17 16:22:16 +0100

    image float clearfix

--- resources/views/float.blade.php
+++ resources/views/float.blade.php
@@ -21,22 +21,27 @@
 	}
 
     img {
-        float : left;
+        float : right;
+        margin-right: 30px;
+    }
+    .clearfix {
+        overflow: auto;
     }
 
 </style>
 <body>
 
 <h1>Simple Float Example</h1>
-
+<div class="clearfix">
 <img src="http://lorempixel.com/400/200/sports" alt="Imatges Lorem">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</div>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias cum et ipsum laboriosam magni, nihil nobis officia praesentium quam quasi, quisquam reprehenderit rerum, sit vel! A accusamus alias consectetur consequuntur dolorem dolores doloribus earum est eum facere illo ipsa ipsum laudantium, maxime minus neque nisi nostrum nulla odit officiis optio perferendis porro quasi qui reprehenderit sequi sint temporibus! Adipisci aliquid cupiditate ex explicabo iste laboriosam minima modi saepe. Aperiam beatae cupiditate dolores dolorum neque pariatur perspiciatis! Aspernatur consectetur eius esse et excepturi fugit laboriosam laudantium minus officiis optio quibusdam, velit veniam vitae? Excepturi in quidem sapiente. Aliquid, obcaecati, odit.
-</p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti excepturi minus mollitia nulla quia quod saepe! A aliquam aspernatur at atque consequatur consequuntur cum dolor dolores earum eius harum, iste itaque maiores minima molestiae nostrum obcaecati odio pariatur perferendis possimus quaerat, quo quod recusandae rem sequi suscipit tempore veritatis vero!
-</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eaque, eum illo labore minima possimus qui sit voluptatem. Corporis, debitis deleniti eius minus nemo perspiciatis reprehenderit tempore temporibus ullam voluptatem. Animi assumenda consequatur deleniti est excepturi. Alias aliquid amet at aut autem blanditiis consequatur corporis dolore dolorem dolorum ipsam iste labore nisi nobis odio officia quam quidem, quis quisquam ratione rem repellendus totam ut velit veritatis vero voluptas. Aut incidunt ipsa odio quibusdam, ratione voluptas!
-</p>
+
+
+
+
 </body>
 </html>
